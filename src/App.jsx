c3741a5cdaba1cbb415afb64da25f1ac7da52b39
gowtham <gowthamallada@gmail.com>
2026-01-28 11:37:35 +0530

    Initial clean commit (CI ready)

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment,decrement,reset } from './store/counterSlice';
+
+function App() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello Redux with Vite 🚀</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      {/* <button onClick={() => dispatch(decrement()) }> Decrease </button>
+      <button onClick={() => dispatch(reset())} >reset</button> */}
+    </div> 
+  )
+}
+
+export default App;
+
